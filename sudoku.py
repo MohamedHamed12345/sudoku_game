99fc0f98c325b93072f2b3fc485ad8379ae8b64a
aya2003A <101410399+aya2003A@users.noreply.github.com>
2023-02-28 16:42:38 +0300

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -1,105 +1,75 @@
-
 import random
-
-BOARD_SIZE = 9
-
-# Define the number of squares to remove for each difficulty level
-EASY_SQUARES = 30
-MEDIUM_SQUARES = 45
-HARD_SQUARES = 60
-EXTREME_SQUARES = 75
-file=open("ahmed.txt","w")
-
-def create_board():
-    board=[]
-    for i in range(9):
-        x=[]
-        for i in range(9):
-            x.append(0)
-        board.append(x)
-    return board
-
-
-def fill_sudoku(sudoku):
-    for row in range(9):
-        for col in range(9):
-            if sudoku[row][col] == 0:
-                nums = list(range(1, 10))
-                random.shuffle(nums)
-                for num in nums:
-                    if (num not in sudoku[row] and 
-                        num not in [sudoku[i][col] for i in range(9)] and
-                        num not in [sudoku[i][j] for i in range(row//3*3, row//3*3+3) for j in range(col//3*3, col//3*3+3)]):
-                        sudoku[row][col] = num
-                        if fill_sudoku(sudoku):
-                            return True
-                        sudoku[row][col] = 0
-                return False
+def ifNumIsValid(sudoku,row,col,num):
+    #function to check if generated number is valid
+    if num in sudoku[row]:
+        return False
+    if num  in [sudoku[i][col] for i in range(0,9)]:
+        return False
+    if num  in [sudoku[i][j] for i in range(row//3*3,row//3*3+3) for j in range(col//3*3,col//3*3+3)]:
+        return False
+    if sudoku[row][col]!=0 :
+        return False 
     return True
-#called in text file by give it board from previous function 
+    
+def genertaeRandom():
+    #this function to generate random numbers in a board from 4 different levels
+    print("choose the level of the game:-\n","1-Easy \n 2-mediam \n 3-hard \n 4-extreme")
+    choice=int(input("your choice : "))
+    sudoku=[[0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            ]
+    position=[0,1,2,3,4,5,6,7,8]
+    if choice ==1 :
+        i=0
+        while i<=34:
+                  row= random.choice(position)
+                  col= random.choice(position)
+                  num=random.randint(1,9)
+                  if(ifNumIsValid(sudoku, row, col, num)==True):
+                      sudoku[row][col]=num
+                      i+=1
+                    
+        print(sudoku)
+    if choice ==2 :
+        i=0
+        while i<=24:
+                  row= random.choice(position)
+                  col= random.choice(position)
+                  num=random.randint(1,9)
+                  if(ifNumIsValid(sudoku, row, col, num)==True):
+                      sudoku[row][col]=num
+                      i+=1
+                  
+        print(sudoku)
+        
+    if choice ==3 :
+        i=0
+        while i<=14:
+                  row= random.choice(position)
+                  col= random.choice(position)
+                  num=random.randint(1,9)
+                  if(ifNumIsValid(sudoku, row, col, num)==True):
+                      sudoku[row][col]=num
+                      i+=1
+        print(sudoku)
+        
+    if choice ==4 :
+        i=0
+        while i<=4:
+                  row= random.choice(position)
+                  col= random.choice(position)
+                  num=random.randint(1,9)
+                  if(ifNumIsValid(sudoku, row, col, num)==True):
+                      sudoku[row][col]=num
+                      i+=1
+        print(sudoku)
+    return sudoku
 
-def write_board(board):
-    # Write the Sudoku board to a text file
-    with open("sudoku_game.txt", 'w') as f:
-        f.write('-' * 35 + '\n')
-        for i in range(BOARD_SIZE):
-            for j in range(BOARD_SIZE):
-                f.write(str(board[i][j]) + '  ')
-                if (j + 1) % 3 == 0 and j != BOARD_SIZE - 1:
-                    f.write('||  ')
-            
-            f.write('\n\n')
-            if (i + 1) % 3 == 0 and i != BOARD_SIZE - 1:
-                f.write('-' * 35 + '\n')
-        f.write('-' * 35 + '\n')
-      
-
-
-def get_difficulty_level():
-    """Get the difficulty level from the user."""
-    while True:
-        level = input('Please select difficulty level (easy, medium, hard, or extreme): ').lower()
-        if level in ['easy', 'medium', 'hard', 'extreme']:
-            return level
-        else:
-            print('Invalid input, please try again.')
-
-
-def remove_squares(board, num_squares):
-    """Remove a specified number of squares from the board."""
-    squares = list(range(BOARD_SIZE ** 2))
-    random.shuffle(squares)
-    for i in range(num_squares):
-        square = squares[i]
-        row = square // BOARD_SIZE  #1 to 9 only
-        col = square % BOARD_SIZE   ##1 to 9 only
-        board[row][col] = "#"
-
-
-
-if __name__=="__main__":    
- board = create_board()
- fill_sudoku(board)
- 
- level = get_difficulty_level()
-
- if level == 'easy':
-    remove_squares(board, EASY_SQUARES)
- elif level == 'medium':
-     remove_squares(board, MEDIUM_SQUARES)
- elif level == 'hard':
-    remove_squares(board, HARD_SQUARES)
- else:
-     remove_squares(board, EXTREME_SQUARES)
-
- write_board(board)
-
-
-
-
-
-
-
-
-
-
+sudoku=genertaeRandom()
